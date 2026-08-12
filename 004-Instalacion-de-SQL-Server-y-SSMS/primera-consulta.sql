fb5cabbbd5d78755f3a9cb1a7ca2e7c05869f992
Jonajubert <124742963+Jonajubert@users.noreply.github.com>
2026-08-11 21:39:21 -0300

    Add initial SQL query to test SQL Server response

---
+++ 004-Instalacion-de-SQL-Server-y-SSMS/primera-consulta.sql
@@ -0,0 +1,13 @@
+/*
+==========================================
+SQL SERVER DESDE CERO
+Capítulo 004
+Instalación de SQL Server y SSMS
+==========================================
+*/
+
+
+-- Mostramos un mensaje para comprobar
+-- que SQL Server responde correctamente.
+
+SELECT '¡SQL Server funciona!' AS Mensaje;
